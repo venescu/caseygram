9fa1d34dac72d27ac0cfbe3053c7299885a4d8ef
Casey DeLange <casey.delange@gmail.com>
2019-07-18 00:56:59 -0500

    updated settings and requirements

--- caseygram/settings.py
+++ caseygram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -160,3 +161,6 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'us-east-2'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+django_heroku.settings(locals())
