9553c5e589e6f5c1586346e559934aab47fabbce
cdelange <casey.delange@gmail.com>
2019-10-01 20:41:39 -0500

    base html rework

--- users/forms.py
+++ users/forms.py
@@ -10,6 +10,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        initial = {'username': 'default@email.com'}
 
 
 class UserUpdateForm(forms.ModelForm):
