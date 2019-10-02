15222630b94106edba381452497c14d009ba10c6
cdelange <casey.delange@gmail.com>
2019-10-01 22:05:58 -0500

    base html rework

--- homepage/views.py
+++ homepage/views.py
@@ -21,7 +21,6 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'homepage/home.html'
     ordering = ['-date_posted']
     context_object_name = 'posts'
-    paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
