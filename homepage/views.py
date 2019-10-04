481b33e111dd8a3ded55ad752922ec3cb32c0b86
cdelange <casey.delange@gmail.com>
2019-10-04 18:05:46 -0500

    explore pagination removed

--- homepage/views.py
+++ homepage/views.py
@@ -20,7 +20,7 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'homepage/home.html'
     ordering = ['-date_posted']
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -194,7 +194,6 @@ class ExploreListView(LoginRequiredMixin, ListView):
     template_name = 'homepage/explore.html'
     ordering = ['-date_posted']
     context_object_name = 'posts'
-    paginate_by = 9
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
