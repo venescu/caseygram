717130a7f5a331397b47a8dc77eb5ca86626a9ce
cdelange <casey.delange@gmail.com>
2019-10-04 09:02:53 -0500

    plz work

--- homepage/forms.py
+++ homepage/forms.py
@@ -27,6 +27,11 @@ class ImageForm(forms.ModelForm):
         model = PostImage
         fields = ['modelimage', ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['label'].widget.attrs.update({'text': ''})
+        # this overwrites the input for content field and lets you update attrs of that input <input>
+
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100)
