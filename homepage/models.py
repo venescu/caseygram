ea13a234a0cfa08598c7332328ccf3a382fb6a2a
cdelange <casey.delange@gmail.com>
2020-09-10 10:46:50 -0500

    Formatting

--- homepage/models.py
+++ homepage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from PIL import Image
+from pillow import Image
 from notifications.signals import notify
 from django.utils.text import Truncator
 
